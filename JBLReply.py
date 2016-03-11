5a001e319f27fdbca5bb1623e0d473d8bd5eca41
ChrisKol <chris@chriskol.com>
2016-03-11 03:39:34 -0400

    Changed gitignore to not up tracked replies. Modified JBLReply to work though more work on limits may be necessary.

--- JBLReply.py
+++ JBLReply.py
@@ -3,7 +3,7 @@ import praw
 import pdb
 import re
 import os
-from jblconfig.py import *
+from jblconfig import *
 
 # Check that the file that contains our username exists
 if not os.path.isfile("jblconfig.py"):
@@ -18,35 +18,35 @@ r = praw.Reddit(user_agent=user_agent)
 r.login(REDDIT_USERNAME, REDDIT_PASS)
 
 # Have we run this code before? If not, create an empty list
-if not os.path.isfile("posts_replied_to.txt"):
-    posts_replied_to = []
+if not os.path.isfile("post_replies.txt"):
+    post_replies = []
 
 # If we have run the code before, load the list of posts we have replied to
 else:
     # Read the file into a list and remove any empty values
-    with open("posts_replied_to.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = filter(None, posts_replied_to)
+    with open("post_replies.txt", "r") as f:
+        post_replies = f.read()
+        post_replies = post_replies.split("\n")
+        post_replies = filter(None, post_replies)
 
 # Get the top 5 values from our subreddit
 subreddit = r.get_subreddit('pythonforengineers')
-for submission in subreddit.get_hot(limit=5):
+for submission in subreddit.get_hot(limit=3):
     # print submission.title
 
     # If we haven't replied to this post before
-    if submission.id not in posts_replied_to:
+    if submission.id not in post_replies:
 
         # Do a case insensitive search
         if re.search("i love python", submission.title, re.IGNORECASE):
             # Reply to the post
-            submission.add_comment("Nigerian scammer bot says: It's all about the Bass (and Python)")
+            submission.add_comment("I LOVE IT!")
             print "Bot replying to : ", submission.title
 
             # Store the current id into our list
-            posts_replied_to.append(submission.id)
+            post_replies.append(submission.id)
 
 # Write our updated list back to the file
-with open("posts_replied_to.txt", "w") as f:
-    for post_id in posts_replied_to:
+with open("post_replies.txt", "w") as f:
+    for post_id in post_replies:
         f.write(post_id + "\n")
